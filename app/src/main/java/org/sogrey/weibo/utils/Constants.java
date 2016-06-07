26c202f0f31a2c1ae7846ac146ea6ad8ad6a87d0
Sogrey <suzhaoheng2006@163.com>
2016-06-07 13:14:14 +0800

    add Welcome

--- app/src/main/java/org/sogrey/weibo/utils/Constants.java
+++ app/src/main/java/org/sogrey/weibo/utils/Constants.java
@@ -10,10 +10,13 @@ public class Constants {
     /**
      * The constant App_Key.
      */
-    public static final String  App_Key="2824359113";
+    public static final String App_Key   ="2824359113";
     /**
      * The constant App_Secret.
      */
-    public static final String  App_Secret="72413a8665961782532190c32ebf71a4";
+    public static final String App_Secret="72413a8665961782532190c32ebf71a4";
 
+    /*SharedPreferences keys*/
+    /** 第一次启动或重大更新后第一次启动 */
+    public static final String SP_KEY_ISFRIST="sp_key_isfrist";
 }
